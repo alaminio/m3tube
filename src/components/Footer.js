ff7344c94f69442658c6a0698ec5872fb599e144
MD AL AMIN <m3alamin@gmail.com>
2020-01-19 11:20:09 +0600

    header, footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,18 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="Footer" id="Footer">
-        <div>
-          <p>&nbsp;</p>
-        </div>
+        <footer className="footer">
+          <div className="container">
+            <div className="columns">
+              <div className="column">
+                Made with <i className="fas fa-heart"></i> by MD AL AMIN
+              </div>
+              <div className="column">
+                <p className="is-pulled-right">Powerd by youtube API v3</p>
+              </div>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -19,21 +19,38 @@ class Header extends Component {
   render() {
     return (
       <div className="Header" id="Header">
-        <div>
-          <div className="field has-addons">
-            <div className="control">
-              <input
-                className="input"
-                type="text"
-                value={this.state.searchQuery}
-                onChange={this.search}
-              />
+        <div class="columns">
+          <div class="column">
+            <div className="field has-addons">
+              <div className="control">
+                <input
+                  className="input"
+                  type="text"
+                  value={this.state.searchQuery}
+                  onChange={this.search}
+                />
+              </div>
+              <div className="control">
+                <a href="/" className="button is-info" onClick={this.submit}>
+                  Search
+                </a>
+              </div>
             </div>
-            <div className="control">
-              <a href="/" className="button is-info" onClick={this.submit}>
-                Search
-              </a>
-            </div>
+          </div>
+          <div class="column">
+            <ul class="menu">
+              <li>m3tube&nbsp;&nbsp;</li>
+              <li class="github">
+                <a
+                  target="_blank"
+                  href="https://github.com/m3alamin/m3tube"
+                  title="View Code On Github"
+                >
+                  <i class="far fa-eye"></i> <i class="fas fa-code"></i> On
+                  <i class="fab fa-github"></i>
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
