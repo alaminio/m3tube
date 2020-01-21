9dcf246a5425282fc2f2c528242dfc087c38af56
m3alamin <m3alamin@gmail.com>
2020-01-21 22:15:43 +0600

    constants file revamped

--- src/config/constants.sample.js
+++ src/config/constants.sample.js
@@ -1,11 +1,14 @@
 export const PLAY_SONG = "PLAY_SONG";
 export const SEARCH_SONG = "SEARCH_SONG";
 export const UPDATE_QUERY = "UPDATE_QUERY";
-export const M3TUBE_KEY = "AIzaSyB-CD2qqObMMMCUthOT1bV8bz32XmZ9pmE";
-// export const M3TUBE_KEY = "AIzaSyCzJ3rS9ATMHTC0suSCA8G0fk0cZ7yWKec";
+/**
+ * Google API Key
+ * Youtube Data Api v3 enabled
+ */ 
+export const M3TUBE_KEY = "";
 export const M3TUBE_URL = "https://www.googleapis.com/youtube/v3";
 export const CHANGE_PAGINATION = "CHANGE_PAGINATION";
 export const SHOW_ERROR = "SHOW_ERROR";
 export const HIDE_ERROR = "HIDE_ERROR";
-export const DEFAULT_VOLUME = 63;
+export const DEFAULT_VOLUME = 10;
 export const BASENAME = "/m3tube";
